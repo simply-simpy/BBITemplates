d8dcf2acc39112e14c25d8fbb1c58e9d0a27bb65
Scott Simpson <>
2018-12-06 15:08:04 -0800

    first push

---
+++ src/outback/gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const connect = require('gulp-connect');
+const sass = require('gulp-sass');
+
+gulp.task('nunjuckTask', function () {
+  return gulp.src('./html/*.html')
+    .pipe(nunjucksRender({
+      path: ['./html/']
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload())
+});
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 8000
+  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(connect.reload());
+});
+
+
+gulp.task('js', function() {
+  return gulp.src('./js/**/**')
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('./fonts/**/**')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('watch', function(){
+  return gulp.watch('./html/**/*', gulp.series('nunjuckTask'));
+});
+
+gulp.task('sass:watch', function () {
+  return gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.parallel('connect','nunjuckTask', 'sass', 'js', 'fonts', 'watch', 'sass:watch'), function (done) {
+  done();
+});
+
